8d087b918342b4a57097e3bd7ddda04c4a0bf1b0
Karan Nautiyal <karannautiyal36@gmail.com>
2020-01-24 11:44:42 +0530

    Add basic code of Crawler

---
+++ src/Crawler/crawl.py
@@ -0,0 +1,22 @@
+import twitter
+
+# Go to http://dev.twitter.com/apps/new to create an app and get values
+# for these credentials, which you'll need to provide in place of these
+# empty string values that are defined as placeholders.
+# See https://developer.twitter.com/en/docs/basics/authentication/overview/oauth
+# for more information on Twitter's OAuth implementation.
+
+CONSUMER_KEY = '2y'
+CONSUMER_SECRET = 'xJ'
+OAUTH_TOKEN = '7y'
+OAUTH_TOKEN_SECRET = '5B'
+
+auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                           CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_api = twitter.Twitter(auth=auth)
+
+# Nothing to see by displaying twitter_api except that it's now a
+# defined variable
+
+print(twitter_api)
