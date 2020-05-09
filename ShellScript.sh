0364764a5116a616812198da0520281098514018
NishkarshRaj <500060720@stu.upes.ac.in>
2020-05-09 17:20:23 +0530

    Shell Script

---
+++ ShellScript.sh
@@ -0,0 +1,27 @@
+# Install Python and configure python
+# sudo apt install -y python3
+# sudo apt install -y python-pip
+# pip install tweepy
+# pip install pandas
+
+# Run The Web Crawler file to generate info.csv
+# python src/crawler/Crawl.py
+
+# Install Java
+# apt install openjdk-11-jre-headless
+
+# Install Maven
+# apt install -y maven
+
+# Maven Builds
+# mvn clean
+# mvn test
+# mvn install
+# mvn site
+
+# Move Crawler output Excel file to Sentimental Analysis workspace
+# mv src/crawler/info.csv src/sentimental_analysis
+
+# Install Machine Learning libraries
+# pip install keras
+# pip install tensorflow
