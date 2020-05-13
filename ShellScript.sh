60363b311a727ea186cc945f4c0730cb4459fecf
NishkarshRaj <500060720@stu.upes.ac.in>
2020-05-13 08:17:50 +0530

    Fix auto accept errors

--- ShellScript.sh
+++ ShellScript.sh
@@ -1,14 +1,14 @@
 # Install Python and configure python
 apt install -y python3
-apt install -y python-pip
-pip install tweepy
-pip install pandas
+apt install -y python3-pip
+pip3 install tweepy
+pip3 install pandas
 
 # Run The Web Crawler file to generate info.csv
-python src/crawler/Crawl.py
+python3 src/crawler/Crawl.py
 
 # Install Java
-apt install openjdk-11-jre-headless
+apt install -y openjdk-11-jre-headless
 
 # Install Maven
 apt install -y maven
@@ -23,8 +23,8 @@ mvn site
 mv src/crawler/info.csv src/sentimental_analysis
 
 # Install Machine Learning libraries
-pip install keras
-pip install tensorflow
+pip3 install keras
+pip3 install tensorflow
 
 # Move to Machine Learning folder from Crawler folder
 cd ..
@@ -41,18 +41,18 @@ read -p "Enter your choice: " ch
 
 # Switch case
 case "$ch" in
-1) python randomforest.py ;;
+1) python3 randomforest.py ;;
 
-2) python decisiontree.py ;;
+2) python3 decisiontree.py ;;
 
-3) python naivebayes.py ;;
+3) python3 naivebayes.py ;;
 
 esac
 
 # Visualization
-apt-get install python3-matplotlib
+apt-get install -y python3-matplotlib
 cd ..
 cd visualization
-python matplotlib.py
+python3 matplotlib.py
 
 
