78e1c90f8f1c4d56411e38d4dc1db2ed0a8afac3
NishkarshRaj <500060720@stu.upes.ac.in>
2020-05-13 08:18:09 +0530

    Create Auto-installation file

---
+++ install.sh
@@ -0,0 +1,19 @@
+# Install Python and configure python
+apt install -y python3
+apt install -y python3-pip
+pip3 install tweepy
+pip3 install pandas
+
+# Install Java
+apt install -y openjdk-11-jre-headless
+
+# Install Maven
+apt install -y maven
+
+# Install Machine Learning libraries
+pip3 install keras
+pip3 install tensorflow
+
+# Visualization
+apt-get install -y python3-matplotlib
+
